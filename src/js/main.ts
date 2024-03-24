b63d644f7a99f7abe77772cc9adfafef86fc668e
julieandersson <juan2301@student.miun.se>
2024-03-24 14:53:31 +0200

    adderar domcontentloaded för att säkerställa att koden körs först när domträdet har laddats, skapar funktion för att hämta sparade kurser från localStorage samt funktion för att spara kuser till localStorage

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,20 +1,29 @@
-// Tilldelning av element till variabeler
-let addButtonEl: HTMLButtonElement | null = document.getElementById("addCourse") as HTMLButtonElement;
-let courseListEl: HTMLElement | null = document.getElementById("courselist") as HTMLDivElement;
-let clearButtonEl: HTMLButtonElement | null = document.getElementById("clearbutton") as HTMLButtonElement;
+document.addEventListener("DOMContentLoaded", function() {
+    const addButtonEl: HTMLButtonElement | null = document.getElementById("addCourse") as HTMLButtonElement;
+    const courseListEl: HTMLElement = document.getElementById("courseList") as HTMLDivElement;
+    const clearButtonEl: HTMLButtonElement | null = document.getElementById("clearbutton") as HTMLButtonElement;
 
-// Definierar strukturen på kursinformationen med interface
-interface courseInfo {
-    name: string;
-    code: string;
-    progression: string;
-    url: string;
-}
+    // Definierar strukturen på kursinformationen med interface
+    interface CourseInfo {
+        name: string;
+        code: string;
+        progression: 'A' | 'B' | 'C';
+        syllabus: string;
+    }
 
-// Händelselyssnare för att lägga till kurs
-document.getElementById("addCourse")?.addEventListener("click", () => {
-    let codeInput = document.getElementById("code") as HTMLInputElement;
-    let nameInput = document.getElementById("name") as HTMLInputElement;
-    let progressionInput = document.getElementById("progression") as HTMLInputElement;
-    let syllabusInput = document.getElementById("syllabus") as HTMLInputElement;
-})
+    // Funktion för att hämta sparade kurser från localStorage
+    function getSavedCourses(): CourseInfo[] {
+        const savedCoursesJSON = localStorage.getItem('courses');
+        if (savedCoursesJSON) {
+            return JSON.parse(savedCoursesJSON);
+        } else {
+            return [];
+        }
+    }
+
+    // Funktion för att spara kurser till localStorage
+    function saveCourses(courses: CourseInfo[]): void {
+        localStorage.setItem('courses', JSON.stringify(courses));
+    }
+
+});
